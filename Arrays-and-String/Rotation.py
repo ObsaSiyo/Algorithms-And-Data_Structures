cfb547d35a361a059768659f5e91b70a998d2dc7
ObsaSiyo <33564367+ObsaSiyo@users.noreply.github.com>
2017-11-12 19:31:26 -0600

    Day3

---
+++ Arrays-and-String/Rotation.py
@@ -0,0 +1,22 @@
+# is s2 a rotation of s1
+
+s1 = "foo"
+s2 = "oof"
+
+s3 = s2 + s2
+
+s4 = "a"
+s5 = "b"
+
+s6 = s5 + s5
+
+
+if s1 in s3:
+    print("True")
+else:
+    print("False")
+
+if s4 in s6:
+    print("True")
+else:
+    print("False")
